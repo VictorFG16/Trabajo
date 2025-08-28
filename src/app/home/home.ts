095061f6086d40eb79815b52fedc9a0c1f21f5d3
ghostP3 <espinosapablo620@gmail.com>
2025-08-28 17:04:05 -0500

    Se implemento Home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../dashboard/navbar/navbar'; // Esta es la ruta correcta
+import { CommonModule } from '@angular/common'; // Agregamos CommonModule para poder usar la navbar
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  // ...
 }
